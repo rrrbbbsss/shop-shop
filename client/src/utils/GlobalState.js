77583803a993d716dada406c2d9db5f2cedb6797
Royce Strange <rrrbbbsss@ryzst.net>
2022-10-18 18:18:13 -0500

    use global store

--- client/src/utils/GlobalState.js
+++ client/src/utils/GlobalState.js
@@ -9,6 +9,8 @@ const StoreProvider = ({ value = [], ...props }) => {
     products: [],
     categories: [],
     currentCategory: "",
+    cart: [],
+    cartOpen: false,
   });
   console.log(state);
   return <Provider value={[state, dispatch]} {...props} />;
